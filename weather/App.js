67f2835d6327328da38e09e74fe43c72b61cea38
Parkjju <rudwns3927@gmail.com>
2022-07-13 00:25:30 +0900

    🐛 fix: aPI KEY 삭제 / data fetch / Ionicons 활용

--- weather/App.js
+++ weather/App.js
@@ -1,85 +1,132 @@
 import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet, ScrollView, Dimensions } from 'react-native';
+import {
+    View,
+    Text,
+    StyleSheet,
+    ScrollView,
+    Dimensions,
+    ActivityIndicator,
+} from 'react-native';
+import { Ionicons } from '@expo/vector-icons';
 import * as Location from 'expo-location';
 
 export default function App() {
-  const [city, setCity] = useState('Loading...');
-  const [location, setLocation] = useState();
-  const [ok, setOk] = useState(true);
-  const ask = async () => {
-    const { granted } = await Location.requestForegroundPermissionsAsync();
+    const [city, setCity] = useState('Loading...');
+    const [days, setDays] = useState([]);
+    const [ok, setOk] = useState(true);
 
-    if (!granted) {
-      setOk(false);
-    }
-    const {
-      coords: { longitude, latitude },
-    } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+    const icons = {
+        Clouds: 'cloudy-outline',
+        Rains: 'ios-rainy-outline',
+    };
 
-    const location = await Location.reverseGeocodeAsync(
-      { latitude, longitude },
-      { useGoogleMaps: false }
+    const getWeather = async () => {
+        const { granted } = await Location.requestForegroundPermissionsAsync();
+
+        if (!granted) {
+            setOk(false);
+        }
+        const {
+            coords: { longitude, latitude },
+        } = await Location.getCurrentPositionAsync({ accuracy: 5 });
+
+        const location = await Location.reverseGeocodeAsync(
+            { latitude, longitude },
+            { useGoogleMaps: false }
+        );
+        setCity(location[0].city);
+        const response = await fetch(
+            `https://api.openweathermap.org/data/2.5/onecall?lat=${latitude}&lon=${longitude}&exclude=alerts&appid=${API_KEY}&units=metric`
+        );
+        const json = await response.json();
+        setDays(json.daily);
+    };
+
+    useEffect(() => {
+        getWeather();
+    });
+
+    return (
+        <View style={styles.container}>
+            <View style={styles.city}>
+                <Text style={styles.cityName}>{city}</Text>
+            </View>
+            <ScrollView
+                pagingEnabled
+                horizontal
+                showsHorizontalScrollIndicator={false}
+                contentContainerStyle={styles.weather}
+            >
+                {days.length === 0 ? (
+                    <View style={styles.day}>
+                        <ActivityIndicator color='white' size='large' />
+                    </View>
+                ) : (
+                    days.map((day, index) => (
+                        <View key={index} style={styles.day}>
+                            <View
+                                style={{
+                                    flexDirection: 'row',
+                                    alignContent: 'center',
+                                }}
+                            >
+                                <Text style={styles.temp}>
+                                    {parseFloat(day.temp.day).toFixed(1)}
+                                </Text>
+                                <Ionicons
+                                    name={icons[day.weather[0].main]}
+                                    size={24}
+                                    color='white'
+                                />
+                            </View>
+                            <Text style={styles.description}>
+                                {day.weather[0].main}
+                            </Text>
+                            <Text style={styles.tinyText}>
+                                {day.weather[0].description}
+                            </Text>
+                        </View>
+                    ))
+                )}
+            </ScrollView>
+        </View>
     );
-    setCity(location[0].city);
-  };
-
-  useEffect(() => {
-    ask();
-  });
-
-  return (
-    <View style={styles.container}>
-      <View style={styles.city}>
-        <Text style={styles.cityName}>{city}</Text>
-      </View>
-      <ScrollView
-        pagingEnabled
-        horizontal
-        showsHorizontalScrollIndicator={false}
-        contentContainerStyle={styles.weather}
-      >
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-        <View style={styles.day}>
-          <Text style={styles.temp}>27</Text>
-          <Text style={styles.description}>Sunny</Text>
-        </View>
-      </ScrollView>
-    </View>
-  );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: 'tomato',
-  },
-  city: {
-    flex: 1.2,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  cityName: {
-    fontSize: 68,
-    fontWeight: '500',
-  },
-  weather: {},
-  day: {
-    width: Dimensions.get('window').width,
-    alignItems: 'center',
-  },
-  temp: {
-    marginTop: 50,
-    fontSize: 178,
-  },
-  description: {
-    marginTop: -30,
-    fontSize: 60,
-  },
+    container: {
+        flex: 1,
+        backgroundColor: 'tomato',
+        color: 'white',
+    },
+    city: {
+        flex: 1.2,
+        justifyContent: 'center',
+        alignItems: 'center',
+    },
+    cityName: {
+        fontSize: 68,
+        fontWeight: '500',
+        color: 'white',
+    },
+    weather: {},
+    day: {
+        width: Dimensions.get('window').width,
+        alignItems: 'flex-start',
+        marginLeft: 20,
+    },
+    temp: {
+        fontSize: 80,
+        color: 'white',
+        fontWeight: '500',
+    },
+    description: {
+        marginTop: 0,
+        fontSize: 40,
+        color: 'white',
+    },
+    tinyText: {
+        fontSize: 20,
+        color: 'white',
+    },
 });
